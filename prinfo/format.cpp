31703d8f2fdfd4a13f0ff4c93617d0bc6a7d2384
Niko Köser <koeserniko@gmail.com>
2018-12-20 01:37:59 +0100

    Add namespace and class and improve overall
    Add namespace Helper. Add class Format. Rename functions. Improve functions.

--- prinfo/format.cpp
+++ prinfo/format.cpp
@@ -1,8 +1,10 @@
 ﻿#include "format.h"
 
+#include <math.h>
 #include <string>
 
-std::wstring format_name_and_value(std::wstring name, std::wstring value) {
+namespace Helper {
+std::wstring Format::name_and_value(std::wstring name, std::wstring value) {
   const int max_length = 50;
 
   if (value.length() < max_length) {
@@ -29,60 +31,71 @@ std::wstring format_name_and_value(std::wstring name, std::wstring value) {
   return (L" " + name + L":" + space + formatted_value);
 }
 
-std::vector<std::wstring> line_wrap(std::wstring value,
-                                    const unsigned max_line_length) {
+std::vector<std::wstring> Format::line_wrap(std::wstring line,
+                                            const unsigned max_line_length) {
   std::vector<std::wstring> lines;
 
-  if (value.length() < 1) {
+  if (line.length() < 1) {
     return lines;
   }
 
-  while (value.length() > max_line_length) {
-    lines.push_back(value.substr(0, max_line_length));
-    value = value.substr(max_line_length, value.length());
+  while (line.length() > max_line_length) {
+    lines.push_back(line.substr(0, max_line_length));
+    line = line.substr(max_line_length, line.length());
   }
 
-  lines.push_back(value.substr(0, max_line_length));
+  lines.push_back(line.substr(0, max_line_length));
 
   return lines;
 }
 
-// Multi_SZ Keys
-std::wstring format_multi_sz_key(HKEY hkey, std::wstring name,
-                                 std::wstring subkey, std::wstring value) {
-  // Größe der Daten
+std::wstring Format::multi_sz_key(HKEY hkey, std::wstring name,
+                                  std::wstring subkey, std::wstring value) {
   std::wstring pfad = name + L"\\" + subkey;
   DWORD size;
   auto result_get_value = RegGetValueW(hkey, pfad.c_str(), value.c_str(),
                                        RRF_RT_ANY, nullptr, nullptr, &size);
 
   if (result_get_value != ERROR_SUCCESS) {
-    return general_error_message(GetLastError());
+    return error_message(GetLastError());
   }
 
+  /**
+    Read data.
+  */
   auto multisz_data = new TCHAR[size / sizeof(TCHAR)];
-
-  // Daten abfragen
   result_get_value = RegGetValueW(hkey, pfad.c_str(), value.c_str(), RRF_RT_ANY,
                                   nullptr, multisz_data, &size);
 
   if (result_get_value != ERROR_SUCCESS) {
     delete[] multisz_data;
-    return general_error_message(GetLastError());
+    return error_message(GetLastError());
   }
 
-  // Multi_SZ String zusammensetzen
+  /**
+  auto multisz = std::wstring(multisz_data);
+
+  for (auto c : multisz) {
+    if (c == '\0') {
+      c = L'\n';
+    }
+  }
+
+  auto l = multisz.length;
+  multisz[l - 1] = L'\0';
+  */
+
+  /**
+    Concat multi_sz_key.
+  */
   std::wstring multisz_string = L"";
   auto multisz_string_length = wcslen(multisz_data);
   auto index = 0;
 
   while (multisz_string_length > 0) {
-    multisz_string += &multisz_data[index];  // den String bis zu \0 auslesen
-                                             // und wMultiszValue �bergeben
-    multisz_string += L" ";  // danach ein Leerzeichen einf�gen; in der Registry
-                             // w�re es ein Absatz
-    index +=
-        multisz_string_length + 1;  // + 1 wegen null-terminated String ( \0 )
+    multisz_string += &multisz_data[index];
+    multisz_string += L" ";
+    index += multisz_string_length + 1;  // '\0' -> + 1
     multisz_string_length = wcslen(&multisz_data[index]);
   }
 
@@ -90,39 +103,37 @@ std::wstring format_multi_sz_key(HKEY hkey, std::wstring name,
   return multisz_string;
 }
 
-// Größe des jeweiligen Jobs umrechnen
-std::wstring format_size_of_job(const DWORD job_size) {
-  // Bytes
+std::wstring Format::byte_conversion(const double job_size) {
   if (job_size < 1024) {
     return std::to_wstring(job_size) + L" B";
   }
-  // KiloBytes
-  if (job_size < 1048576) {
+  if (job_size < pow(1024, 2)) {
     return std::to_wstring(job_size / 1024) + L" KB";
   }
-  // MegaBytes
-  if (job_size < 1073741824) {
-    return std::to_wstring(job_size / 1048576) + L" MB";
+  if (job_size < pow(1024, 3)) {
+    return std::to_wstring(job_size / pow(1024, 2)) + L" MB";
   }
-  // GigaBytes
-  if (job_size < 1099511627776) {
-    return std::to_wstring(job_size / 1073741824) + L" GB";
+  if (job_size < pow(1024, 4)) {
+    return std::to_wstring(job_size / pow(1024, 3)) + L" GB";
   }
-  // TeraBytes
-  if (job_size < 1125899906842624) {
-    return std::to_wstring(job_size / 1099511627776) + L" TB";
+  if (job_size < pow(1024, 5)) {
+    return std::to_wstring(job_size / pow(1024, 4)) + L" TB";
   }
 
   return L"Zu groß";
 }
 
-std::wstring general_error_message(DWORD error_message_id) {
-  // No error
+std::wstring Format::error_message(DWORD error_message_id) {
+  /**
+    Check if is error.
+  */
   if (error_message_id == 0) {
     return std::wstring();
   }
 
-  // Format error-message-id to wstring
+  /**
+    Format error-message-id to wstring.
+  */
   LPWSTR error_message_buffer = nullptr;
   const auto size = FormatMessageW(
       FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM |
@@ -131,14 +142,11 @@ std::wstring general_error_message(DWORD error_message_id) {
       reinterpret_cast<LPWSTR>(&error_message_buffer), 0, NULL);
 
   std::wstring error_message(error_message_buffer, size);
-
-  // Free the buffer
   LocalFree(error_message_buffer);
-
   return error_message;
 }
 
-void handle_exception(std::exception_ptr exception_pointer) {
+void Format::handle_exception(std::exception_ptr exception_pointer) {
   try {
     if (exception_pointer) {
       std::rethrow_exception(exception_pointer);
@@ -150,3 +158,4 @@ void handle_exception(std::exception_ptr exception_pointer) {
     _getwch();
   }
 }
+}  // namespace Helper
--- prinfo/format.h
+++ prinfo/format.h
@@ -4,12 +4,57 @@
 #include <iostream>
 #include <vector>
 
-// Text formatieren
-std::wstring format_name_and_value(std::wstring name, std::wstring value);
-std::vector<std::wstring> line_wrap(std::wstring value,
-                                    const unsigned max_line_length);
-std::wstring format_multi_sz_key(HKEY hkey, std::wstring name,
-                                 std::wstring subkey, std::wstring value);
-std::wstring format_size_of_job(DWORD job_size);
-std::wstring general_error_message(DWORD error_message_id);
-void handle_exception(std::exception_ptr exception_pointer);
+namespace Helper {
+class Format {
+ public:
+  /**
+    Format name value pair so that the overall output stream
+    looks very similar.
+
+    @param name The name to be put in formatted string.
+    @param value The value to be put in formatted string.
+  */
+  static std::wstring name_and_value(std::wstring name, std::wstring value);
+
+  /**
+    Multi_sz keys are a sequence of null-terminated strings terminated by an
+    empty string. (e.g. String1\0String2\0String3\0\0)
+    If we would simlpy print this string to console we would only see 'String1'.
+    We have to replace the null-terminations ('\0') with blanks (" ").
+  */
+  static std::wstring multi_sz_key(HKEY hkey, std::wstring name,
+                                   std::wstring subkey, std::wstring value);
+
+  /**
+    Converts bytes in more readable size units like KB, MB...
+
+    @param job_size The number to be converted.
+  */
+  static std::wstring byte_conversion(double job_size);
+
+  /**
+    Formats a system error code to a equvilant readable error string.
+
+    @param error_message_id The id of the system error.
+  */
+  static std::wstring error_message(DWORD error_message_id);
+
+  /**      Handle an exception, print out the exception.
+
+    @param exception_pointer Pointer to the previously occured exception.
+  */
+  static void handle_exception(std::exception_ptr exception_pointer);
+
+ private:
+  /**
+    Splits a line in sub-lines all with a defined max length.
+    Serves as formatting function for long strings. A long string will be
+    displayed as block not one long line.
+
+    @param line The line to split.
+    @param max_line_length Defines the maximum allowed number of chars per line.
+  */
+  static std::vector<std::wstring> line_wrap(std::wstring line,
+                                             const unsigned max_line_length);
+};  // class Format
+}  // namespace Helper
