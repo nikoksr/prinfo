b190ca758398a69b0409bc092d15f9555534a6ce
Niko Köser <koeserniko@gmail.com>
2019-04-12 12:02:40 +0200

    Transform static class snippets to namespace

--- prinfo/src/snippets.cpp
+++
@@ -1,9 +0,0 @@
-﻿#include "snippets.hpp"
-
-namespace Helper {
-    // Static member initialization
-    constexpr wchar_t Snippets::program_head[];
-    constexpr wchar_t Snippets::help[];
-    constexpr wchar_t Snippets::separator_thick[];
-    constexpr wchar_t Snippets::separator_thin[];
-}  // namespace Helper
