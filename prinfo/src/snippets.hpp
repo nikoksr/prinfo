e01cd53c665ff3216df026f74b90d8a402f497fd
Niko Köser <koeserniko@gmail.com>
2019-09-11 19:22:45 +0200

    Update help message and header
    - Version number was deprecated and remote url was pointing to old gitlab
    repo.
    - Set version number to current v0.2.0 and set url to github repo.
    - Nothing is affected by this change.

--- prinfo/src/snippets.hpp
+++ prinfo/src/snippets.hpp
@@ -10,7 +10,7 @@ namespace snippets {
     * Includes Copyright, Edit date, Creator, Contact email.
     */
     constexpr wchar_t k_program_head[] =
-        L"\n Prinfo v0.3        01.04.2019        http://192.168.10.250/admins/prinfo        koeserniko@gmail.com";
+        L"\n Prinfo v0.2.0        11.09.2019        https://github.com/nikoksr/prinfo        koeserniko@gmail.com";
 
     /**
     * Help text.
@@ -22,14 +22,14 @@ namespace snippets {
         L"  daher nicht für seine Fehlerlosigkeit und Stabilität, so wie es\n"
         L"  auch in der Lizens zu lesen ist.\n\n\n"
         L" GitHub\n\n"
-        L"  http://192.168.10.250/admins/prinfo\n\n\n"
+        L"  https://github.com/nikoksr/prinfo\n\n\n"
         L" Lizens\n\n"
         L"  GNU GPLv3\n\n\n"
         L" Kontakt\n\n"
         L"  koeserniko@gmail.com\n\n\n"
         L" Wichtige Hinweise\n\n"
         L"  - Gewisse Funktionen brauchen eventuell Admin-Rechte.\n\n"
-        L"  - Bei Formatierungsproblemen der gespeicherten Daten in Notepad:\n"
+        L"  - Bei Darstellungsproblemen der gespeicherten Daten in Notepad:\n"
         L"    Schriftart auf 'Consolas', Schriftschnitt auf 'Standard' und\n"
         L"    Schriftgröße auf '11' umstellen.\n\n";
 
