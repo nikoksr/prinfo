9f677e9275a6a37b078f4d15d9c52a2642da7386
Niko Köser <koeserniko@gmail.com>
2019-09-11 18:48:43 +0200

    Export display and save functions
    See e5dee25c2f51687484c124e27373e90846a7b806 and e3c4f7addce614b6eb4319f7ad6bf553aa6a0ca1 for more information. Remove unecessary UndoBack function. Apply new naming style - see d92005fffd1f3b603c72a9f7a1958ec9e7d59118 .

--- prinfo/src/menu.hpp
+++ prinfo/src/menu.hpp
@@ -2,48 +2,30 @@
 
 #include <string>
 #include <sstream>
+#include <memory>
 
-namespace Helper {
-    class Menu {
+namespace menu {
+    class Functions;
+
+    class Main {
     public:
-        /**
-          Main menu.
-        */
-        static void main_menu();
+        virtual wchar_t Show();
+    private:
+        std::unique_ptr<Functions> m_funcMenu;
+    };
 
-        /**
-          Menu for listing all available functions.
-        */
-        static wchar_t functions_menu();
+    class Functions : Main {
+    public:
+        wchar_t Show();
+    };
 
+    class Navigation {
+    public:
         /**
           Submenus for navigating back, quitting and saving data.
         */
-        static wchar_t submenu_quit();
-        static wchar_t submenu_back_quit();
-        static wchar_t submenu_save_back_quit();
-
-        /**
-          Save functionality.
-
-          @param function_to_save Pointer to function which data is wanted to be
-            saved.
-          @param user_input Char that user put in console to execute function. Used
-            for stability. Check if input was actually a call to save data.
-        */
-        static void saveToFile(void(*function_to_save)(std::wostream &stream));
-
-        /**
-          TODO: Find out what function does.
-        */
-        static void undo_back_command(wchar_t *user_input);
-
-        // Display functions
-        static void display_all(std::wostream& wos);
-        static void display_winapi_printers(std::wostream& wos);
-        static void display_registry_printers(std::wostream& wos);
-        static void display_system(std::wostream& wos);
-        static void display_help(std::wostream& wos);
-
-    };  // class Menu
-}  // namespace Helper
+        static wchar_t Quit();
+        static wchar_t BackQuit();
+        static wchar_t SaveBackQuit();
+    };
+}
