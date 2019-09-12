2407d8d656ecd9b5426fe4d9d7ee0233441fc6d8
Niko Köser <koeserniko@gmail.com>
2019-09-13 01:09:11 +0200

    Add function to analyze the file count and distribution
    - Part of feature request #9.
    - Add a class to represent basic file. Read and evaluate all files from
    PRINTERS folder. Categorize them into .SPH, .SPL and junk files. Print
    the distribution of the found files.
    - Main menu option 1 now displays the file distribution in PRINTERS.

--- prinfo/src/analyze.cpp
+++ prinfo/src/analyze.cpp
@@ -2,22 +2,75 @@
 #include "snippets.hpp"
 #include "format.hpp"
 
+#include <locale>
+#include <codecvt>
+
+#include <fstream>
+#include <iostream>
+#include <filesystem>
+
 namespace analyze {
-    PrintersFolder::PrintersFolder() : m_num_of_files(countFiles(path)) {}
 
-    const std::size_t PrintersFolder::NumOfFiles() const { return m_num_of_files; }
+    // File
+    File::File(fs::path path, uintmax_t size, fs::file_time_type last_write_time) :
+        m_path(path),
+        m_size(size),
+        m_last_write_time(last_write_time) {}
 
-    std::size_t PrintersFolder::countFiles(std::filesystem::path path) {
-        using std::filesystem::directory_iterator;
-        using fp = bool(*)(const std::filesystem::path&);
-        return std::count_if(directory_iterator(path), directory_iterator{}, (fp)std::filesystem::is_regular_file);
+    std::wostream& operator<<(std::wostream& stream, const File& file) {
+        stream << file.Path();
+        return stream;
     }
 
+    const fs::path File::Path() const { return m_path; }
+    const uintmax_t File::Size() const { return m_size; }
+    const fs::file_time_type File::LastWriteTime() const { return m_last_write_time; }
+
+    // PrintersFolder
+    PrintersFolder::PrintersFolder() {
+        getFiles(path);
+    }
+
+    const std::size_t PrintersFolder::NumSpoolFiles() const { return m_num_spool_files; }
+
     std::wostream& operator<<(std::wostream& stream, const PrintersFolder& p_folder) {
-        stream << L" Analyse " << p_folder.path << "\n"
+        stream << snippets::k_separator_thick << L"\n\n"
+            << L" Analyse " << p_folder.path << "\n"
             << snippets::k_separator_thin << L"\n\n";
 
-        stream << Format::NameValuePair(L"Dateien", std::to_wstring(p_folder.m_num_of_files)) << L"\n\n";
+        stream << Format::NameValuePair(L"Dateien Gesamt", std::to_wstring(p_folder.m_total_num_files)) << L"\n"
+            << Format::NameValuePair(L"Spool Dateien", std::to_wstring(p_folder.m_num_spool_files)) << L"\n"
+            << Format::NameValuePair(L"Junk Dateien", std::to_wstring(p_folder.m_num_junk_files)) << L"\n\n";
         return stream;
+    }
+
+    std::size_t PrintersFolder::countFiles(fs::path path) {
+        using fs::directory_iterator;
+        using fp = bool(*)(const fs::path&);
+        return static_cast<std::size_t>(std::count_if(directory_iterator(path), directory_iterator{}, static_cast<fp>(fs::is_regular_file)));
+    }
+
+    void PrintersFolder::getFiles(fs::path path) {
+        m_spool_files.clear();
+        m_total_num_files = countFiles(path);
+
+        if (m_total_num_files < 1) {
+            return;
+        }
+
+        m_spool_files.reserve(m_total_num_files);
+
+        for (const auto& entry : fs::directory_iterator(path)) {
+            // Spooler file
+            if (Format::EndsWith(entry.path(), L".SHD")
+                || Format::EndsWith(entry.path(), L".SPL")) {
+                m_spool_files.push_back(std::make_unique<File>(entry.path(), entry.file_size(), entry.last_write_time()));
+                m_num_spool_files += 1;
+                continue;
+            }
+
+            // Junk file
+            m_num_junk_files += 1;
+        }
     }
 }
--- prinfo/src/analyze.hpp
+++ prinfo/src/analyze.hpp
@@ -2,8 +2,32 @@
 
 #include <filesystem>
 #include <sstream>
+#include <vector>
+#include <memory>
 
 namespace analyze {
+
+    namespace fs = std::filesystem;
+
+    class File {
+        // File represents a file in the filesystem. Mostly used to hold critical data of files.
+
+    public:
+
+        File(fs::path path, uintmax_t size, fs::file_time_type last_write_time);
+
+        friend std::wostream& operator<<(std::wostream& stream, const File& file);
+
+        const fs::path Path() const;
+        const uintmax_t Size() const;
+        const fs::file_time_type LastWriteTime() const;
+
+    private:
+
+        fs::path m_path;
+        uintmax_t m_size;
+        fs::file_time_type m_last_write_time;
+    };
 
     class PrintersFolder {
         // A class to analyze the C:\Windows\System32\spool\PRINTERS folder
@@ -14,15 +38,18 @@ namespace analyze {
 
         PrintersFolder();
 
-        const std::size_t NumOfFiles() const;
+        const std::size_t NumSpoolFiles() const;
 
     private:
-
-        std::size_t m_num_of_files;
+        std::size_t m_total_num_files;
+        std::size_t m_num_spool_files;
+        std::size_t m_num_junk_files;
+        std::vector<std::unique_ptr<File>> m_spool_files;
 
         static constexpr wchar_t path[] = L"C:\\Windows\\System32\\spool\\PRINTERS";
 
         // Count the number of files in the folder
-        std::size_t countFiles(std::filesystem::path path);
+        std::size_t countFiles(fs::path path);
+        void getFiles(fs::path path);
     };
 }
